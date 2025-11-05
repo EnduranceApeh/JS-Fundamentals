babf76219a9a06c8751ab8be7838bec07099d8bc
EnduranceApeh <apehendurance2@gmail.com>
2025-11-05 05:10:18 -0800

    Added 2-arguments.js file

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+// A script that print a message depending on the number of args passed
+const { argv } = require('node:process');
+
+const args = argv.slice(2);
+if(args.length === 0) {
+	console.log('No argument');
+}else if(args.length === 1) {
+	console.log('Argument found');
+}else {
+	console.log('Arguments found')
+}
