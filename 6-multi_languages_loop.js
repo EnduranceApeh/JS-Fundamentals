a04953d8c1bf464611b006c835db2190c0e0b3b2
EnduranceApeh <apehendurance2@gmail.com>
2025-11-05 07:17:53 -0800

    Added 6-multi_languages_loop.js file

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+// A script that pint out the value of an Array.
+
+const stringArr = ['C is fun', 'Python is amazing', 'JavaScript is amazing'];
+
+let i = 0;
+while(i < stringArr.length) {
+	console.log(stringArr[i])
+	i++;
+}
